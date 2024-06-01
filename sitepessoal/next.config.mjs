a033fd26572685a447c478f0cb7e4d2cf6f34ebc
WesleyMirand <wewesantos@hotmail.com>
2024-05-31 22:22:09 -0300

    4

--- sitepessoal/next.config.mjs
+++ sitepessoal/next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  }
 };
 
 export default nextConfig;
